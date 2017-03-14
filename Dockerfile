79bf4b7a016fa0c8506ceebf6f39a456ef854ea4
Gowda, Manjesh (mg153v) <mg153v@us.att.com>
2017-03-14 10:34:59 -0400

    Dockerized application

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:8
-RUN mkdir -p /opt/ganesh/geolocation
+RUN mkdir -p /opt/ganesh/geolocation/data
+RUN chmod 777 /opt
 ADD target/geolocation-0.0.1-SNAPSHOT.jar /opt/ganesh/geolocation/
 EXPOSE 8080
 CMD ["java", "-jar", "/opt/ganesh/geolocation/geolocation-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/manjesh/microservices/repository/GeoLocationRepository.java
+++ src/main/java/com/manjesh/microservices/repository/GeoLocationRepository.java
@@ -1,8 +1,10 @@
 package com.manjesh.microservices.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manjesh.microservices.model.GeoLocation;
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,8 +25,20 @@ public class GeoLocationRepository {
                 new GeoLocation(1, 2, UUID.randomUUID(), System.currentTimeMillis()));
     }
 
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final String DATA_FILES_DIR =
+            System.getenv("GEOLOCATION_DATA_FILES_DIR") != null ?
+                    System.getenv("GEOLOCATION_DATA_FILES_DIR") :
+                    "/opt/ganesh/geolocation/data";
+
     public void addGeoLocation(GeoLocation geolocation) {
         geolocations.add(geolocation);
+        try {
+            MAPPER.writeValue(new File(DATA_FILES_DIR + "/user" + geolocation.getUserId() + "_t" +
+                    geolocation.getTimestamp()), geolocation);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public List<GeoLocation> getGeoLocations() {
